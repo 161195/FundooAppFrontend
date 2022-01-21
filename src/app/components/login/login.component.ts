18c2bb4aec7d4e80b4dee3c2385564f84502c6e4
“mayuri” <mayurisalunkhe01@gmail.com>
2022-01-21 23:00:12 +0530

    Fundoo Application added api integration to login user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/service/userService/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   submitted = true;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,private user: UserService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -18,7 +19,23 @@ export class LoginComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]],
   });
   }
-  onSubmit(){
-    console.log(this.loginForm.value);
+  onSubmit() { 
+    this.submitted=true;
+    if(this.loginForm.valid)
+    {
+      console.log(this.loginForm.value);
+      let login={
+       emailId:this.loginForm.value.email,
+       password:this.loginForm.value.password,
+      
+     }
+     this.user.userLogin(login).subscribe((response:any)=>{
+       console.log(response)
+     })
+    }
+    else
+    {
+      console.log("invalid");
+    }
   }
 }
--- src/app/service/httpService/http.service.ts
+++ src/app/service/httpService/http.service.ts
@@ -8,7 +8,12 @@ export class HttpService {
   baseUrl='https://localhost:44317/api'
 
   constructor(private http:HttpClient) { }
-  postService(url:any,data:any,token:boolean=false,httpOptions:any){
+  // postService(url:any,data:any,token:boolean=false,httpOptions:any){
+  //   return this.http.post(this.baseUrl+url,data,token && httpOptions)
+  // }
+
+  postService(url:any,data:any,token:boolean=true,httpOptions:any){
     return this.http.post(this.baseUrl+url,data,token && httpOptions)
   }
+
 }
